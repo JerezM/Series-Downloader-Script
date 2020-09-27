3fd88cf2b3c98b8a36e6a64756c55fcdaa199665
JerezM <jerez149@gmail.com>
2020-09-26 21:07:50 -0300

    def downloadfile(name, url) added

--- script.py
+++ script.py
@@ -1,1 +1,18 @@
-print("hello world!")
+import requests
+
+URL = "http://download2341.mediafire.com/np1fuwdg7vug/jzvatwj4039ixai/yahorr-12.mp4"
+
+def downloadfile(name,url):
+    name=name+".mp4"
+    r=requests.get(url)
+    print ("****Connected****")
+    f=open(name,'wb')
+    print ("Downloading.....")
+    for chunk in r.iter_content(chunk_size=255): 
+        if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+    print ("Done")
+    f.close()
+
+downloadfile("yahorr-12",URL)
+
